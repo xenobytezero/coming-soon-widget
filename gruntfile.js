61dfed128828c5249778df066d62eb9f95463309
Unknown <xenobytezero@gmail.com>
2018-09-03 02:24:32 +0100

    Deploy Stuff
    * Added gruntfile for deploying
    * Moving to actual version of widget-support

---
+++ gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+
+    let deployDest = 'deploy/<%= pkg.version %>/coming-soon-widget/';
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-exec');
+
+    // Project configuration.
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        copy: {
+        
+            deploy: {
+                files: [
+
+                    // PHP
+                    {src: ['index.php'], dest: deployDest},
+                    {src: ['src/**/*.php'], dest: deployDest},
+
+                    // Composer
+                    {src: ['composer.json', 'composer.lock'], dest: deployDest},
+                    {src: ['vendor/**/*'], dest: deployDest},
+
+                    // Templates
+                    {src: ['templates/**/*.twig'], dest: deployDest},
+
+                    // Other
+                    {
+                        src: [
+                            'README.md',
+                            'LICENCE'
+                        ], 
+                        dest: deployDest
+                    }
+                ]
+            }
+        },
+
+        bump: {
+            options: {
+                push:false
+            }
+        },
+
+        replace: {
+            deployedVersionTag: {
+
+                src: [deployDest + '/index.php'],
+                dest: deployDest + '/index.php',
+
+                options: {
+                    patterns: [{
+                        match: 'releaseVersion',
+                        replacement: '<%= pkg.version %>'
+                    }]
+                }
+            }
+        }
+
+
+    });
+
+    grunt.registerTask('readpkg', 'Read in the package.json file', function() {
+        grunt.config.set('pkg', grunt.file.readJSON('./package.json'));
+    });
+
+    // Default task(s).
+    grunt.registerTask('build', []);
+    grunt.registerTask('dryRelease', ['build', 'copy:deploy', 'replace:deployedVersionTag'])
+    grunt.registerTask('release', ['bump', 'readpkg', 'dryRelease']);
+  
+  };
